daca978b8bc366dd9d3a18ca347d1f6dc8c235b6
Manfred Steyer <manfred.steyer@gmx.net>
2026-03-20 11:31:44 +0100

    fix: remove unneccesary imports

--- app/shell/sidebar/sidebar.ts
+++ app/shell/sidebar/sidebar.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [],
   templateUrl: './sidebar.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
